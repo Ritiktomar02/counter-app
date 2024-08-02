727d13bc72200f40399c06781083f991ac5e823f
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2024-08-02 13:58:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    
+  const [count,setcount]=useState(0);
+  return(
+    
+    <div className=" bg-gray-800 w-screen h-screen flex-col gap-y-5 flex justify-center items-center">
+
+      <div className="text-xs text-blue-400">Increament & Decrement</div>
+
+      <div className="flex justify-around w-[200px] py-3 bg-white rounded-sm font-bold text-2xl">
+        <button onClick={()=>setcount(count+1)} >+</button>
+        <div className=" border-l-2 border-r-2  border-gray-300 h-full px-8">{count}</div>
+        <button onClick={()=>setcount(count-1)}>-</button>
+      </div>
+
+      <button onClick={()=>setcount(0)} className="px-6 bg-blue-400 text-white py-1 rounded-sm">Reset</button>
     </div>
-  );
+  )
+
 }
 
 export default App;
